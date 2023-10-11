a3d30e4a1358f4f1b7834badccf6b2c4f3c51b73
MrRyhor <125206451+MrRyhor@users.noreply.github.com>
2023-10-11 17:46:59 +0200

    homework lesson-2

---
+++ utils/data.js
@@ -0,0 +1,55 @@
+export const data = [
+  { id: "1", imgSrc: "../img/table.png", eng: "table", translate: "stół" },
+  { id: "2", imgSrc: "../img/chair.png", eng: "chair", translate: "krzesło" },
+  { id: "3", imgSrc: "../img/car.png", eng: "car", translate: "samochód" },
+  { id: "4", imgSrc: "../img/window.png", eng: "window", translate: "okno" },
+];
+
+export const workerList = [
+  { id: "1", name: "Ivanov", salary: 10000 },
+  { id: "2", name: "Petrov", salary: 10200 },
+  { id: "3", name: "Savchenko", salary: 20000 },
+  { id: "4", name: "Bubnov", salary: 13000 },
+  { id: "5", name: "Zelenko", salary: 11000 },
+  { id: "6", name: "Dudko", salary: 14700 },
+];
+
+export const searchList = [
+  {
+    id: "1",
+    logo: "https://w7.pngwing.com/pngs/854/555/png-transparent-vue-js-hd-logo-thumbnail.png",
+    name: "vue.org",
+    link: "https://vuejs.org/",
+    title: "The Progressive JavaScript Framework",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+  },
+  {
+    id: "2",
+    logo: "https://cdn.icon-icons.com/icons2/2699/PNG/512/stackoverflow_tile_logo_icon_167969.png",
+    name: "stackoverflow.com",
+    link: "https://stackoverflow.com/",
+    title: "Every developer has a tab open to Stack Overflow",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+  },
+  {
+    id: "3",
+    logo: "https://w7.pngwing.com/pngs/403/269/png-transparent-react-react-native-logos-brands-in-colors-icon-thumbnail.png",
+    name: "react.dev",
+    link: "https://react.dev/",
+    title: "The library for web and native user interfaces",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+  },
+  {
+    id: "4",
+    logo: "https://nodejs.org/static/images/logo-hexagon-card.png",
+    name: "node.js",
+    link: "https://nodejs.org/",
+    title:
+      "Node.js® — это кросс-платформенная среда выполнения JavaScript с открытым исходным кодом.",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+  },
+];
